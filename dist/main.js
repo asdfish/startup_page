2ab2ca0d7d18514dace37c98bd986575faf234e8
asdfish <lgray3420@gmail.com>
2024-11-06 14:24:54 -0500

    added particles

--- dist/main.js
+++ dist/main.js
@@ -1,3 +1,63 @@
 "use strict";
 (function () {
+    var canvas = document.getElementsByTagName("canvas")[0];
+    var ctx = canvas.getContext("2d");
+    var font = "EnvyCodeR";
+    var font_height = 20;
+    ctx.font = String(font_height) + "px " + font;
+    var font_width = ctx.measureText("a").width;
+    var grid_width = 0;
+    var grid_height = 0;
+    var resize = function () {
+        grid_width = Math.ceil(innerWidth / font_width);
+        grid_height = Math.ceil(innerHeight / font_height);
+        canvas.width = innerWidth;
+        canvas.height = innerHeight;
+    };
+    resize();
+    addEventListener("resize", resize);
+    var FireParticle = /** @class */ (function () {
+        function FireParticle() {
+            var _this = this;
+            this.x = 0;
+            this.y = 0;
+            this.health = 30;
+            this.alive = true;
+            this.update = function () {
+                _this.health--;
+                if (_this.health < 0)
+                    _this.alive = false;
+            };
+            this.reset = function () {
+                _this.x = Math.round(Math.random() * grid_width);
+                _this.y = canvas.height;
+                _this.health = 30;
+                _this.alive = true;
+            };
+            this.reset();
+        }
+        return FireParticle;
+    }());
+    var grid = [];
+    var particles = [];
+    var ticks = 0;
+    var add_new_particle = function () {
+        for (var i = 0; i < particles.length; i++)
+            if (!particles[i].alive) {
+                particles[i].reset();
+                return;
+            }
+        particles.push(new FireParticle());
+    };
+    var loop = function () {
+        if (ticks === 30) {
+            add_new_particle();
+            ticks = 0;
+        }
+        for (var i = 0; i < particles.length; i++)
+            particles[i].update();
+        ticks++;
+        requestAnimationFrame(loop);
+    };
+    requestAnimationFrame(loop);
 })();
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,77 @@
 (() => {
+  const canvas: HTMLCanvasElement = document.getElementsByTagName("canvas")![0];
+  const ctx: CanvasRenderingContext2D = canvas.getContext("2d")!;
+  
+  const font: string = "EnvyCodeR";
+  const font_height: number = 20;
 
+  ctx.font = String(font_height) + "px " + font;
+
+  const font_width: number = ctx.measureText("a").width;
+
+  let grid_width: number = 0;
+  let grid_height: number = 0;
+
+  const resize = (): void => {
+    grid_width = Math.ceil(innerWidth / font_width);
+    grid_height = Math.ceil(innerHeight / font_height);
+
+    canvas.width = innerWidth;
+    canvas.height = innerHeight;
+  };
+  resize();
+  addEventListener("resize", resize);
+
+  class FireParticle {
+    x: number = 0;
+    y: number = 0;
+    health: number = 30;
+    alive: boolean = true;
+
+    constructor () {
+      this.reset();
+    }
+
+    update = (): void => {
+      this.health --;
+      if(this.health < 0)
+        this.alive = false;
+    }
+
+    reset = (): void => {
+      this.x = Math.round(Math.random() * grid_width);
+      this.y = canvas.height;
+      this.health = 30;
+      this.alive = true;
+    }
+  }
+
+  let grid: number[][] = [];
+  let particles: FireParticle[] = [];
+
+  let ticks: number = 0;
+
+  const add_new_particle = (): void => {
+    for(let i = 0; i < particles.length; i ++)
+      if(!particles[i].alive) {
+        particles[i].reset();
+        return;
+      }
+
+    particles.push(new FireParticle());
+  }
+
+  const loop = (): void => {
+    if(ticks === 30) {
+      add_new_particle();
+      ticks = 0;
+    }
+
+    for(let i = 0; i < particles.length; i ++)
+      particles[i].update();
+
+    ticks ++;
+    requestAnimationFrame(loop);
+  }
+  requestAnimationFrame(loop);
 })();
